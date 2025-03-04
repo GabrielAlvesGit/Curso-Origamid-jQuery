3d533b386ff79ed5eb2b096d64b4d04a683bea1f
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-03-03 22:15:15 -0300

    initial

---
+++ 2-Fundamentos-do-JavaScript/2-1-Variáveis/js/app.js
@@ -0,0 +1,11 @@
+var x = 30;
+var y = 70;
+var resultado = x + x + x * y / x;
+
+// console.log(resultado); 
+
+for (var i = 0; i <= 100; i++, i % 2) {
+
+  console.log('Números Pares: '+  i)
+
+}
---
+++ 2-Fundamentos-do-JavaScript/2-2-Tipos-de-Variaveis/js/app.js
@@ -0,0 +1,42 @@
+var nome = "Maria";
+var idade = 50;
+var faculdade = true;
+
+console.log(typeof nome);
+
+var nome1 = "Gabriel ";
+var sobrenome = "Alves";
+var nomeCompleto = nome1 + sobrenome;
+
+console.log(nomeCompleto);
+
+// Soma
+var gols = 1000;
+var frase = "Romário Fez " + gols + "GOLS";
+
+console.log(frase);
+
+// Aspas duplas
+var empresa1 = "Apple";
+var empresa2 = "Google" + " Teste 'teste' aqui" ;
+
+console.log(empresa2);
+
+// Decimal
+var pi = 3.14;
+var raio = 5;
+var area = pi * raio * raio;
+
+// NAN = Not Number
+var numero = 80;
+var unidade = "kg";
+var peso = numero + unidade;
+
+console.log(typeof peso);
+
+//
+var faculdade = true;
+
+if (faculdade){
+  console.log("Você está matriculado");
+}
