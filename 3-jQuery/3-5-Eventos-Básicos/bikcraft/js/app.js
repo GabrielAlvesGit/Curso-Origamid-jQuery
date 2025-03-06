775242731d1a0ed625b9424e5f429ba2f7492790
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-03-05 22:42:33 -0300

    manipulaçao do Dom Inside e Outside

---
+++ 3-jQuery/3-5-Eventos-Básicos/bikcraft/js/app.js
@@ -0,0 +1,10 @@
+$('p').after('<span> . </span>');
+
+// Add um after em todos os p
+$('p').insertAfter('<span> Aquiiiiiiiiiiiiiiiiii </span>');
+
+// Add um before em todos os p
+$('p').insertBefore('<span> . </span>');
+
+// Em todos os Parágrafos 
+$('p').wrap('<span> . </span>');
